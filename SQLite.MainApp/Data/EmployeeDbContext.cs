ba5784c35c038f86becc10aca082df98446a5be7
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2023-11-19 00:05:24 +0200

    lesson two. Database setup

---
+++ SQLite.MainApp/Data/EmployeeDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SQLite.MainApp.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public EmployeeDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(Configuration.GetConnectionString("DatabaseMain"));
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
